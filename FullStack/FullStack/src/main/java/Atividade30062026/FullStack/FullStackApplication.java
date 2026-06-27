62c5ed81462a5c200258bca8b5ca4ed64a6b97d1
xstephaniexsouzax <stephaniesis024@gmail.com>
2026-06-27 17:32:11 -0300

    estrutura do backend

---
+++ FullStack/FullStack/src/main/java/Atividade30062026/FullStack/FullStackApplication.java
@@ -0,0 +1,13 @@
+package Atividade30062026.FullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackApplication.class, args);
+	}
+
+}
---
+++ FullStack/FullStack/src/test/java/Atividade30062026/FullStack/FullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package Atividade30062026.FullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
